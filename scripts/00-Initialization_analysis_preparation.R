7b64f65407282050bfb9aa845b4acf5c463be774
Andrew Habrich <andrhabr@gmail.com>
2022-09-27 14:54:43 -0400

    setup

---
+++ scripts/00-Initialization_analysis_preparation.R
@@ -0,0 +1,6 @@
+#Use this package to communicate with Github through R
+install.packages("usethis") 
+library(usethis)
+
+#Update any personal authentication token required
+gitcreds::gitcreds_set()
