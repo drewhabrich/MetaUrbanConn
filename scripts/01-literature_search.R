dc644d7db48ddfca77f3037909f4b2b105ecf8f6
drewhabrich <andrewhabrich@cmail.carleton.ca>
2022-09-28 17:26:04 -0400

    litsearchr facilitating search strategy development for urban connectivity meta-analysis.

---
+++ scripts/01-literature_search.R
@@ -0,0 +1,6 @@
+
+# Literature search preparation -------------------------------------------
+install.packages('remotes') #lets you download R packages outside of CRAN.
+library(remotes)
+install_github("elizagrames/litsearchr", ref="main")
+library(litsearchr)
