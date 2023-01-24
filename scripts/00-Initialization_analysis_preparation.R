102a445a9db04fc44e7fa4503d8c9f4e93da62b3
Andrew Habrich <andrhabr@gmail.com>
2023-01-23 19:30:20 -0500

    Initial updates

--- scripts/00-Initialization_analysis_preparation.R
+++ scripts/00-Initialization_analysis_preparation.R
@@ -1,7 +1,8 @@
 #Use this package to communicate with Github through R
 install.packages("usethis") 
+install.packages("tidyverse")
 library(usethis)
+library(tidyverse)
 
 #Update any personal authentication token required
 gitcreds::gitcreds_set() #don't need to do this every time, only when initializing on a new computer
-
--- scripts/01-literature_search.R
+++ scripts/01-literature_search.R
@@ -1,7 +1,29 @@
-
 # Literature search preparation -------------------------------------------
-install.packages('remotes') #lets you download R packages outside of CRAN.
-library(remotes)
-install_github("elizagrames/litsearchr", ref="main")
+library(remotes) #install.packages('remotes') #lets you download R packages outside of CRAN. Use as needed
+remotes::install_github("rmetaverse/metaverse") #this installs a whole set of meta-analysis related R packages, https://rmetaverse.github.io/
+#Relevant packages; litsearchr, revtools, synthesisr, metaDigitise, robvis. Load as needed.
+library(tidyverse)
 library(litsearchr)
+library(wordcloud)
 
+# Importing results from initial search string ----------------------------
+naive_results<-import_results(directory="raw_data/naive_search") #Downloads all files in folder if no specific file is referenced
+#check the data structure/format
+str(naive_results)
+colnames(naive_results)
+
+# Generating potential search terms
+## from keywords
+naive_results$keywords %>% is.na() %>% sum() #351 results missing keywords
+extract_terms(keywords = naive_results$keywords, 
+              method="tagged", #keywords used by authors in the article
+              min_freq = 5, #Only keywords used >5 times
+              min_n = 1, 
+              max_n = 4) #1-4 word long keywords
+### visualize it in a wordcloud?
+
+## from Titles and Abstracts
+extract_terms(text=naive_results[, "title"], 
+              method="fakerake", #using a modified RAPID AUTOMATIC KEYWORD EXTRACTION
+              min_freq=3, #
+              min_n=2)
