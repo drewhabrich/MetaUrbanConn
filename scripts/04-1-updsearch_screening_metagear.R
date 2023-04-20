17acaae084d010224eb2f69889a391c1db7c6c0d
Andrew Habrich <andrhabr@gmail.com>
2023-04-20 13:42:51 -0400

    Updated final screening csv

--- scripts/04-1-updsearch_screening_metagear.R
+++ scripts/04-1-updsearch_screening_metagear.R
@@ -37,8 +37,8 @@ prv.screen <- prv.screen %>% select(!c("end_page","publisher","issue")) #remove 
 
 ### merge based on title
 merged_df <- upd.screen %>%
-  left_join(prv.screen %>% select(title, INCLUDE, doi, url), by = "title", suffix = c("_upd", "_prv")) %>%
-  mutate(INCLUDE = ifelse(!is.na(INCLUDE_prv), INCLUDE_prv, INCLUDE_upd)) %>% 
+  left_join(prv.screen %>% select(title, INCLUDE, doi, url), by = "title", suffix = c("_upd", "_prv")) %>% #select only relevant columns
+  mutate(INCLUDE = ifelse(!is.na(INCLUDE_prv), INCLUDE_prv, INCLUDE_upd)) %>% #create new INCLUDE column, if NA in previous, replace with updated 
   relocate(INCLUDE, .after = "REVIEWERS") %>% 
   select(-INCLUDE_upd, -INCLUDE_prv)
 
@@ -48,7 +48,7 @@ df_yes <- merged_df %>% filter(INCLUDE == "YES")
 df_myb <- merged_df %>% filter(INCLUDE == "MAYBE")
 
 ## Write the merged dataframe to csv for screening
-#write_csv(merged_df, "./output/upd_screening_effort-04-1.csv", col_names = T) #uncomment if needed, only need to do it once
+write_csv(merged_df, "./output/upd_screening_effort-04-1.csv", col_names = T) #uncomment if needed, only need to do it once
 
 ## Write a csv for each group in the previously screened, for revision later.
 # write_csv(df_unscreened, "./output/unscreenedbib-04-1.csv", col_names = T)
@@ -171,4 +171,27 @@ bibscreened %>%
   theme(axis.text.y = element_text(size = 5)) + #specific modifications to the theme
   scale_y_discrete(labels = function(x) str_trunc(x, 35)) +
   scale_x_continuous(limits = c(0, 120), breaks = seq(0, 120, 5), expand=expand_scale(mult=c(0,0.1))) 
+
+# 5. Full screening efforts -----------------------------------------------
+updscreening <- read_csv("./output/upd_screening_effort-04-1.csv", col_names = T)
+updscreening %>% filter(INCLUDE == "not vetted") %>% count("not vetted") #there should be as many rows as bibscreened df
+
+fullscreen <- updscreening %>%
+              left_join(bibscreened %>% select(STUDY_ID, INCLUDE), by = "STUDY_ID") %>% # join the two dataframes
+              mutate(INCLUDE = if_else(INCLUDE.x == "not vetted", INCLUDE.y, INCLUDE.x)) %>%  #replace 'not vetted' from unscreened entries with the decisions from the upd screening
+              relocate(INCLUDE, .after = "REVIEWERS") %>% 
+              select(-INCLUDE.x, -INCLUDE.y)
+
+fullscreen %>% 
+  group_by(INCLUDE) %>% #group by category
+  summarize(count=n()) %>% #summarize based on count of each category
+  mutate(percentage = count/nrow(fullscreen)*100, #estimate the percentage in each category
+         summary = case_when(INCLUDE == "NOtitle" ~ "Excluded based title, no indication of either urban/city or connectivity assessed",
+                             INCLUDE == "NOabstr" ~ "Excluded based on abstract, either not urban/city or no metric of connectivity",
+                             INCLUDE == "MAYBE" ~ "Unclear eligibility, needs full text screening",
+                             INCLUDE == "YES" ~ "Candidate studies identified",
+                             INCLUDE == "REVIEW" ~ "Review/Methodology/Framework articles",
+                             TRUE ~ "IN PROGRESS, not vetted yet")) %>% 
+  arrange(desc(percentage))
+
 # TEST ZONE ----
